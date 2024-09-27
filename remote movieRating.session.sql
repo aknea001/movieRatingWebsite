727dcd00200690533b71ea3a108c4ed4c896c5eb
Aksel <aknea001@osloskolen.no>
2024-09-27 09:38:40 +0200

    testing adding ratings to title pages

--- remote movieRating.session.sql
+++ remote movieRating.session.sql
@@ -14,9 +14,28 @@ VALUES
 -- @block
 SELECT
     movies.title,
-    ratings.rating,
-    users.email
+    movies.genre,
+    ratings.rating
 FROM ratings
-INNER JOIN movies on ratings.movieID = movies.id
-INNER JOIN users on ratings.userID = users.id
-ORDER BY movieID ASC;
+INNER JOIN movies ON ratings.movieID = movies.id
+WHERE movieID = 1
+ORDER BY ratings.movieID ASC;
+
+-- @block
+SELECT movies.title, movies.genre, ratings.rating FROM ratings INNER JOIN movies ON ratings.movieID = movies.id WHERE movieID = 4;
+
+-- @block
+SELECT movies.title, movies.genre, GROUP_CONCAT(ratings.rating ORDER BY ratings.rating ASC) AS all_ratings FROM movies INNER JOIN ratings ON ratings.movieID = movies.id WHERE ratings.movieID = 2 GROUP BY movies.id, movies.title, movies.genre;
+
+
+-- @block
+INSERT INTO ratings (movieID, rating, userID)
+VALUES
+    (1, 5, 3),
+    (1, 3, 4),
+    (1, 1, 5),
+    (1, 5, 6);
+
+-- @block
+SELECT * FROM ratings
+WHERE movieID = 1
